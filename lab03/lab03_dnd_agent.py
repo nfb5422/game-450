0df371d3a9adb319c0bbc48242393ead2cef4ec2
nfb5422 <nfb5422@psu.edu>
2025-01-28 11:43:35 -0500

    I worked to make the chat work and ask about the users role

--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -6,10 +6,16 @@ from ollama import chat
 from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
-sign_your_name = 'Pulin Agrawal'
-model = ''
-options = {}
-messages = []
+sign_your_name = 'Noah Basinger'
+model = 'llama3.2'
+options = {'temperature': 0.5, 'max_tokens': 50}
+messages = [
+  {'role': 'system', 'content': 'You should have DnD knowledge like a dungeon master \
+                                 and be able to respond to moves a DnD party  makes\
+                                 and first lest the user pick their class and then develops their stats \
+                                 and makes NPCs to fill out the party based on the users class.'},
+  
+]
 
 
 # But before here.
@@ -19,8 +25,12 @@ options |= {'seed': seed(sign_your_name)}
 while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # Add your code below
-
-
+  
+  response = chat(model=model, messages=messages, stream=False, options=options)
+  messages.append({'role': 'assistant', 'content': response.message.content})
+  print(f'DM: {response.message.content}')
+  message = {'role': 'user', 'content': input('You: ')}
+  messages.append(message)
   # But before here.
   if messages[-1]['content'] == '/exit':
     break
